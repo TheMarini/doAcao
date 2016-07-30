24a171c0e5967450a0e9756d1387dc2c2fd70e88
vinikis <viniciusqueiroga@live.com>
2016-07-30 04:27:10 -0300

    Estruturação do MVC Completa

--- index.php
+++ index.php
@@ -8,9 +8,14 @@ require_once './controllers/common.php';
 /* -- Trocador de página -- */
 
 $url = isset($_GET['pag'])?htmlspecialchars($_GET['pag']):null;
+$id = isset($_GET['id'])?htmlspecialchars($_GET['id']):null;
 
 if(!(is_null($url) || empty($url))){
-    
+    if(file_exists("./views/$url.php") && !($url == 'footer') && !($url == 'header')){
+        include "./views/$url.php";
+    }else{
+        include "./views/erro404.php";
+    }
 }else{
     include "./views/index.php";
 }
