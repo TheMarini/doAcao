aea235d89eba7c187e6981f70efa902ee3f3589c
Vinícius Teixeira <viniciusqueiroga@live.com>
2016-11-29 08:02:27 -0200

    Backup

--- prjDoacao/template/js/mercadoria.js
+++ prjDoacao/template/js/mercadoria.js
@@ -31,17 +31,29 @@ function loadTipoMercadoria(){
         url: "mercadoria/novo",
         success: function(result){
             TipoMercadoria = result;
-            $("#unid").empty();
-            $('#tipos').empty();
-            TipoMercadoria.forEach(function(tpmec){
-                $('#tipos').append('<option value="'+tpmec.codigo+'">'+ tpmec.nome +'</option>');
-            });
-
         },
         error: function(){
             alert('Erro ao realizar requisição Ajax');
         }
         
+    });
+}
+
+function searchTipoMercadoria(termo = ""){
+    $('#tipos').empty();
+    if(termo != ""){
+        var rgxp = new RegExp(termo, "g");
+        TipoMercadoria.forEach(function(item){
+            if(item.nome.match(rgxp)){
+                $('#tipos').append('<li value="'+item.codigo+'">'+item.nome+'</li>');
+            }
+            
+        });
+        return;
+    }
+
+    TipoMercadoria.forEach(function(item){
+       $('#tipos').append('<li value="'+item.codigo+'">'+item.nome+'</li>');  
     });
 }
 
@@ -56,20 +68,51 @@ $(document).ready(function(){
     //onLoad
     listarItens();
 
+    //document click evnt
+    $(document).click(function(evt){
+        if(!$(evt.target).is('#tipoMercadoria')){
+            $('#tipos').hide();
+        }
+    });
+
     //blackcover click
     $('#blackcover').click(function (evt){
         toggleAddItem();
     });
-    
+
     //btnAdicionar evnt
     $('#btnAdicionar').click(function(evt){
         toggleAddItem();
         loadTipoMercadoria();
     });
 
+    //btnVoltar evnt
+    $('#btnVoltar').click(function(evt){
+        toggleAddItem();
+    });
+
+    /* ---Adicionar Mercadoria---- */
+
+    //tipo de mercadoria keydown event
+    $('#tipoMercadoria').keyup(function(evt){
+        if(!(evt.which == 37 || evt.which == 38 || evt.which == 39 || evt.which == 40)){ 
+            $(this).removeClass('selected');
+            searchTipoMercadoria($(this).val());
+            $('#tipos').show();
+        }        
+    });
+    //seleciona a opcao
+    $(document).on('click','#tipos > li' ,function(evt){
+        $('#tipoMercadoria').val($(this).html());
+        $('#tipoMercadoria').addClass('selected');
+        $('#cdTipoMercadoria').val($(this).val());
+        $('#tipos').hide();
+        $("#tipoMercadoria").trigger('change');
+    });
+
     //unid change evnt
     $("#tipoMercadoria").change(function(){
-        cd = $(this).val();
+        cd = $('#cdTipoMercadoria').val();
         unidades = [];
         TipoMercadoria.forEach(function(item){
             if(item.codigo == cd){
