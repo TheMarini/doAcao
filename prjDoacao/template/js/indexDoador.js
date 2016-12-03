4427e9d1b426a340b27c1c22776ee4b0d2700d91
Bruno Marini <bruno.marini@hotmail.com>
2016-12-03 19:10:22 -0200

    Feed (90%)

--- prjDoacao/template/js/indexDoador.js
+++ prjDoacao/template/js/indexDoador.js
@@ -1,10 +1,29 @@
 /* -- when main doc is ready --*/
 $('document').ready(function () {
-    var altura = parseInt($('header').height()) + parseInt($('header').css('padding')) +  parseInt($('header').css('padding')) + parseInt($('#conquistas').css('height')) + parseInt($('svg').css('padding')) + parseInt($('#first-post').css('height')) + parseInt($('#first-post').css('margin-bottom'));
+
+    /*First post together to header*/
+    var altura = parseInt($('header').height()) + parseInt($('header').css('padding')) + parseInt($('header').css('padding')) + parseInt($('#conquistas').css('height')) + parseInt($('svg').css('padding')) + parseInt($('#first-post').css('height')) + parseInt($('#first-post').css('margin-bottom'));
 
     $('#newsfeed').css('height', altura);
-    
-    $('.stat-container').click(function(){
-        $(this).css('height','200px');
+
+    /*Expand aside*/
+    var AlturaOriginal = parseInt($('.stat-container').css('height'));
+    var AlturaReduzida = AlturaOriginal - 67;
+
+    $('.stat-container').css('height', AlturaReduzida);
+
+    $('.stat-container').click(function () {
+        $(this).css('height', AlturaOriginal);
+        $(this).css('border-bottom', '2px solid gray');
+    });
+
+    $('.stat-container').mouseover(function () {
+        $(this).css('height', AlturaOriginal);
+        $(this).css('border-bottom', '2px solid gray');
+    });
+
+    $('.stat-container').mouseout(function () {
+        $(this).css('height', AlturaReduzida);
+        $(this).css('border-bottom', 'none');
     });
 });
